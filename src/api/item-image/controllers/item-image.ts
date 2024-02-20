8c5de261d8fb87c80690fae56f6da19ea3d94ad8
Sam Edge <guitarfella@gmail.com>
2024-02-20 17:00:07 +1030

    Add item image types

---
+++ src/api/item-image/controllers/item-image.ts
@@ -0,0 +1,7 @@
+/**
+ * item-image controller
+ */
+
+import {factories} from '@strapi/strapi'
+
+export default factories.createCoreController('api::item-image.item-image');
---
+++ src/api/item-image/routes/item-image.ts
@@ -0,0 +1,7 @@
+/**
+ * item-image router
+ */
+
+import {factories} from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::item-image.item-image');
